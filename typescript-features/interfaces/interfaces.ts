c8cacf4f58865f9a5b57b798c67e7d0d79c15fbb
escobard <escobard90@gmail.com>
2023-11-23 11:21:25 -0700

    feat: add sample interface annotation

---
+++ typescript-features/interfaces/interfaces.ts
@@ -0,0 +1,27 @@
+
+interface Vehicle {
+    name: string;
+    year: number;
+    broken: boolean;
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true
+};
+
+// very long annotation, how do we fix this? interface!
+/*const printVehicle = (vehicle: { name: string; year: number; broken: boolean }) => {
+    console.log(`Name: ${vehicle.name}`);
+    console.log(`Year ${vehicle.year}`);
+    console.log(`Broken? ${vehicle.broken}`);
+};*/
+
+const printVehicle = (vehicle: Vehicle): void => {
+    console.log(`Name: ${vehicle.name}`);
+    console.log(`Year ${vehicle.year}`);
+    console.log(`Broken? ${vehicle.broken}`);
+};
+
+printVehicle(oldCivic);
