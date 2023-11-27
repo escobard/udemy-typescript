90597146bc0db1f3b484634456db43d5ca23b13e
escobard <escobard90@gmail.com>
2023-11-27 16:28:08 -0700

    feat: add markerContent function to User and Company class

--- typescript-random-gmap-marker/src/CustomMap.ts
+++ typescript-random-gmap-marker/src/CustomMap.ts
@@ -3,7 +3,8 @@ interface Mappable {
   location: {
     lat: number;
     lng: number;
-  }
+  },
+  markerContent(): string;
 }
 
 // carries a reference to google maps, with limited access only
@@ -36,7 +37,7 @@ export class CustomMap {
     // event listener for when the marker gets clicked
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: 'Hi there!'
+        content: mappable.markerContent()
       })
       infoWindow.open(this.googleMap, marker)
     })
--- typescript-random-gmap-marker/src/User.ts
+++ typescript-random-gmap-marker/src/User.ts
@@ -13,4 +13,8 @@ export class User {
       lng: faker.address.longitude()
     }
   }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`
+  }
 }
