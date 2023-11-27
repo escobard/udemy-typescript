6b2fa9010718056b4a91715077f96679bab9dafe
escobard <escobard90@gmail.com>
2023-11-27 14:04:09 -0700

    feat: add company class

--- typescript-random-gmap-company/src/Company.ts
+++ typescript-random-gmap-company/src/Company.ts
@@ -1,13 +1,16 @@
 import { faker } from "@faker-js/faker";
 
-export class User {
-  name: string;
+export class Company {
+  companyName: string;
+  catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   }
-  constructor() {
-    this.name = faker.name.firstName();
+
+  constructor(){
+    this.companyName = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: faker.address.latitude(),
       lng: faker.address.longitude()
