4d31f4387d21c99de437fea98c525f087ed1ce28
escobard <escobard90@gmail.com>
2023-12-01 09:20:59 -0700

    feat: add LinkedList sort to index

--- typescript-sorting-algorithm/build/index.js
+++ typescript-sorting-algorithm/build/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
 const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedList_1 = require("./LinkedList");
 // numbers sorting
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0, 20, -32]);
 // sorter now expects an instance of numbersCollection class as constructor argument
@@ -14,3 +15,12 @@ const charactersCollection = new CharactersCollection_1.CharactersCollection('Xa
 const charSorter = new Sorter_1.Sorter(charactersCollection);
 charSorter.sort();
 console.log('string sorting:', charactersCollection.data);
+// linkedlist sorting
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const linkedSorter = new Sorter_1.Sorter(linkedList);
+linkedSorter.sort();
+linkedList.print();
