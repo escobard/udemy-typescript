16f8c2b8076d9f65fa6b98cf58b92e2cb09d5890
escobard <escobard90@gmail.com>
2023-12-04 13:42:28 -0700

    feat: add logic to check to see how many wins Manchester United had

--- typescript-stats/build/index.js
+++ typescript-stats/build/index.js
@@ -17,4 +17,13 @@ const matches = fs_1.default.readFileSync('./sampleData/football.csv', {
     .map((row) => {
     return row.split(',');
 });
-console.log(matches);
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man united' && match[5] === 'H') {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- typescript-stats/src/index.ts
+++ typescript-stats/src/index.ts
@@ -12,5 +12,15 @@ const matches = fs.readFileSync('./sampleData/football.csv', {
 // tells readFileSync how to parse each line of data
   .map((row: string): string[] => {
     return row.split(',')
-})
-console.log(matches)
+});
+
+let manUnitedWins = 0;
+for (let match of matches) {
+  if (match[1] === 'Man united' && match[5] === 'H'){
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A'){
+    manUnitedWins++
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
