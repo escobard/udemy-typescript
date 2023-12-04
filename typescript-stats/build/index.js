b06e027e0c874fb8c9c61a736bef28531dc53f86
escobard <escobard90@gmail.com>
2023-12-04 13:33:58 -0700

    feat: add logic to find and prase csv file into a string[][]

--- typescript-stats/build/index.js
+++ typescript-stats/build/index.js
@@ -1,2 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+// types must be defined for all imported libraries to avoid ts errors
+const fs_1 = __importDefault(require("fs"));
+// grabs csv file and parses its content to a string
+const matches = fs_1.default.readFileSync('./sampleData/football.csv', {
+    // tells readFileSync what kind of content we will expect from the file
+    /// returns a string from the file's content
+    encoding: 'utf-8'
+})
+    // tells readFileSync how to identify each row of data
+    .split('\n')
+    // tells readFileSync how to parse each line of data
+    .map((row) => {
+    return row.split(',');
+});
+console.log(matches);
--- typescript-stats/src/index.ts
+++ typescript-stats/src/index.ts
@@ -1,2 +1,16 @@
 // types must be defined for all imported libraries to avoid ts errors
 import fs from 'fs';
+
+// grabs csv file and parses its content to a string
+const matches = fs.readFileSync('./sampleData/football.csv', {
+  // tells readFileSync what kind of content we will expect from the file
+  /// returns a string from the file's content
+  encoding: 'utf-8'
+})
+// tells readFileSync how to identify each row of data
+  .split('\n')
+// tells readFileSync how to parse each line of data
+  .map((row: string): string[] => {
+    return row.split(',')
+})
+console.log(matches)
