e3a87343ffd05b21b2331db668d43550b04d003c
escobard <escobard90@gmail.com>
2023-12-11 14:51:51 -0700

    feat: add MatchResult enum and apply to modules

--- typescript-stats/src/index.ts
+++ typescript-stats/src/index.ts
@@ -1,16 +1,9 @@
 // grabs csv file and parses its content to a string
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult"
 
 const reader = new CsvFileReader('football.csv')
 reader.read()
-
-// basic enum syntax
-/// can be referred to just like an object - eg, MatchResult.HomeWin
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
 
 let manUnitedWins = 0;
 for (let match of reader.data) {
