281a25e449e736716297129c2efce629f228f87b
escobard <escobard90@gmail.com>
2023-12-12 13:24:26 -0700

    feat: add Summary class and matchReader load into index.js file

--- typescript-stats/src/index.ts
+++ typescript-stats/src/index.ts
@@ -10,5 +10,11 @@ const csvFileReader = new CsvFileReader('football.csv');
 
 // create an instance of MatchReader and pass in something satisfying the DataReader interface
 const matchReader = new MatchReader(csvFileReader);
+matchReader.load()
 
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+)
+
+summary.buildAndPrintReport(matchReader.matches);
