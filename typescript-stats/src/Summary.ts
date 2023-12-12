3282aaab8079152154dc3542eb88e7e6837359e0
escobard <escobard90@gmail.com>
2023-12-12 13:18:40 -0700

    feat: add build and print report method, invoking outputTarget class

--- typescript-stats/src/Summary.ts
+++ typescript-stats/src/Summary.ts
@@ -8,6 +8,12 @@ export interface OutputTarget {
   print(report: string): void;
 }
 
+// builds a generic class that can output different kinds of information based on the type that it is constructed with - very cool!
 export class Summary {
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget){}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output)
+  }
 }
--- typescript-stats/src/index.ts
+++ typescript-stats/src/index.ts
@@ -1,24 +1,14 @@
 // grabs csv file and parses its content to a string
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
-import { MatchResult } from "./MatchResult"
+import { ConsoleReport } from "./reportTargets/ConsoleReports";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
 
 // create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
 
 // create an instance of MatchReader and pass in something satisfying the DataReader interface
 const matchReader = new MatchReader(csvFileReader);
-
-// loads the file's data
-matchReader.load();
-
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin){
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
-    manUnitedWins++
-  }
-}
 
 console.log(`Man United won ${manUnitedWins} games`);
