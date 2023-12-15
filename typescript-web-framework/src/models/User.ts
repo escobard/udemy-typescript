c741fe34ebdf9b13a6e4a6156ff9c4fade7ef73e
escobard <escobard90@gmail.com>
2023-12-15 16:09:59 -0700

    feat: section13, lesson 152, on method to model/User and Callback type alias

--- typescript-web-framework/src/models/User.ts
+++ typescript-web-framework/src/models/User.ts
@@ -5,7 +5,16 @@ interface UserProps{
   name?: string;
   age?: number;
 }
+
+type Callback = () => {}
+
 export class User {
+
+  events: {
+    // sample type annotation for a key string type
+    /// each object has a key that contains an array of Callback functions
+    [key: string]: Callback[]
+  } = {}
   // good to extract deep nested type annotations into an interface
   /// eg, { name: string; age: number } to UserProps interface
   constructor(private data: UserProps){}
@@ -16,6 +25,12 @@ export class User {
   set(update: UserProps): void {
     // copies new object properties over to original object
     Object.assign(this.data, update);
+  }
+
+  // expects a callback function, so a function invocation to be passed
+  /// type annotation is a big confusing, so extracting into ts alias Callback
+  on(eventName: string, callback: Callback){
+
   }
 }
 
