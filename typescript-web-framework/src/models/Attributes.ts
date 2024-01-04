4117f5ce1251411abf6a0dd9c17b0b79e17abee7
escobard <escobard90@gmail.com>
2024-01-04 11:59:26 -0700

    feat: section13, lesson 172-173, generics with functions

--- typescript-web-framework/src/models/Attributes.ts
+++ typescript-web-framework/src/models/Attributes.ts
@@ -1,7 +1,9 @@
 export class Attributes<T extends object> {
   constructor(private data: T){}
   // retrieves data from this.data by using propName key as argument
-  get(propName: string): (number | string) {
+  /// you can use generics with function definitions too, syntax below
+  //// T[K] passes in the K generic into the T generic - K must contain a key that exists in T to work
+  get<K extends keyof T>(key: K): T[K] {
     return this.data[propName]
   }
   set(update: T): void {
