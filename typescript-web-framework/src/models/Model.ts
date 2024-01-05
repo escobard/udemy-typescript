038de4f1a2d456a788e915dc1b5a63c4ae5301de
escobard <escobard90@gmail.com>
2024-01-05 16:40:06 -0700

    feat: section13, lesson 185, set up Model class and refactor methods from User

--- typescript-web-framework/src/models/Model.ts
+++ typescript-web-framework/src/models/Model.ts
@@ -1,4 +1,4 @@
-import { AxiosPromise } from "axios";
+import { AxiosPromise, AxiosResponse } from "axios";
 
 // interfaces are recommended to make the Model class more re-usable in the future
 interface ModelAttributes<T> {
@@ -17,6 +17,53 @@ interface Events {
   trigger(eventName: string): void;
 }
 
-export class Model {
+// can use an interface to extend a generic model with additional types T must contain
+interface HasId {
+  id?: number
+}
+
+export class Model<T extends HasId> {
+  constructor(private attributes: ModelAttributes<T>, private events: Events, private sync: Sync<T>){}
+
+  // allows for invocation of the on method from events, from the User class, without calling this.events.on() function directly
+  /// get prefix allows for calling a function without actually having to invoke it or pass arguments
+  /// get functions do not require type declarations
+  get on(){
+    return this.events.on;
+  }
+
+  get trigger(){
+    return this.events.trigger;
+  }
+  get get(){
+    return this.attributes.get;
+  }
+  set(update: T): void {
+    this.attributes.set(update);
+    this.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse) => {
+      // call this.set from the parent class instead of attributes.set, since we want to trigger a change update
+      this.set(response.data);
+    })
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((): void => {
+        this.trigger('save');
+      })
+      .catch((): void => {
+        this.trigger('error');
+      });
+
+  }
 
 }
--- typescript-web-framework/src/models/User.ts
+++ typescript-web-framework/src/models/User.ts
@@ -1,8 +1,4 @@
-import { AxiosResponse } from "axios";
-
-import { Eventing } from './Eventing'
-import { Sync } from "./Sync"
-import { Attributes } from "./Attributes";
+import { Model } from "./Model";
 
 export interface UserProps{
   // name? syntax marks properties as optional in ts
@@ -15,52 +11,6 @@ export interface UserProps{
 // URL to JSON server
 const rootUrl = 'http://localhost:3000/users'
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-  constructor(attrs: UserProps){
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
+export class User extends Model<UserProps> {
 
-  // allows for invocation of the on method from events, from the User class, without calling this.events.on() function directly
-  /// get prefix allows for calling a function without actually having to invoke it or pass arguments
-  /// get functions do not require type declarations
-  get on(){
-    return this.events.on;
-  }
-
-  get trigger(){
-    return this.events.trigger;
-  }
-  get get(){
-    return this.attributes.get;
-  }
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id');
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id');
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse) => {
-      // call this.set from the parent class instead of attributes.set, since we want to trigger a change update
-      this.set(response.data);
-    })
-  }
-
-  save(): void {
-    this.sync.save(this.attributes.getAll())
-      .then((): void => {
-        this.trigger('save');
-      })
-      .catch((): void => {
-        this.trigger('error');
-      });
-
-  }
 }
