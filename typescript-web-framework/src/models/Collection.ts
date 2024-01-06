1a4f18319b23a0de6fd5ea80cd9512364258c7c4
escobard <escobard90@gmail.com>
2024-01-05 20:18:25 -0700

    feat: section13, lesson 190, add Collection class

--- typescript-web-framework/src/models/Collection.ts
+++ typescript-web-framework/src/models/Collection.ts
@@ -1,13 +1,33 @@
-import { User } from "./User";
+import axios, {AxiosResponse} from 'axios';
+
+import { User, UserProps } from "./User";
 import { Eventing } from "./Eventing";
 
-
+// act as a browser cache to store data from api
 export class Collection {
+
+  constructor(public rootUrl: string){}
+
   models: User[] = [];
   events: Eventing = new Eventing();
 
   // if initializing a function with static declarators (not as constructor arguments), we have to use the get syntax instead of function = this.child.function
   get on(){
     return this.events.on;
+  }
+
+  get trigger(){
+    return this.events.trigger;
+  }
+
+  fetch(): void {
+    // loop through all available users and save them to the collection
+    axios.get(this.rootUrl).then((response: AxiosResponse) => {
+      response.data.forEach((value: UserProps) => {
+        const user = User.buildUser(value);
+        this.models.push(user);
+      })
+    })
+    this.trigger('change')
   }
 }
