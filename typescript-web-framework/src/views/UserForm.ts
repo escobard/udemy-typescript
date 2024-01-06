571201f2e823e89d3b276e57aa2e34e5532bfe4e
escobard <escobard90@gmail.com>
2024-01-06 12:19:19 -0700

    feat: section13, lesson 196, add template and HTML to UserForm class

--- typescript-web-framework/src/views/UserForm.ts
+++ typescript-web-framework/src/views/UserForm.ts
@@ -2,13 +2,23 @@ export class UserForm {
   // can determine HTML specific types with TS, cool!
   /// Element type refers to any HTML that we want to render
   //// there are more specific HTML types to explore
-  parent: Element
+  constructor(parent: Element) {}
+
 
   template(): string{
     return `
       <div>
-      
+        <h1>User Form</h1>
+        <input />
       </div>
-    `
+    `;
+  }
+
+  render(): void {
+    // creates an HTML template with raw JS, this is what helps make React so powerful
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.parent.append(templateElement.content);
   }
 }
