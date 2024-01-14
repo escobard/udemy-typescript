2640d9be19434c802ee4a462157f53ad5d045bff
escobard <escobard90@gmail.com>
2024-01-14 14:14:29 -0700

    build: section 14, lesson 222, create basic HTML route and get app to listen on port 3000

--- typescript-with-express/src/index.ts
+++ typescript-with-express/src/index.ts
@@ -1,1 +1,20 @@
-console.log('hi there')
+// need a type definition file to work with express and ts
+/// reminder that most libraries need a companion type definition to work with TS
+//// in extreme cases, libraries do not have TS types and we have to build our own
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+// important to ask - how far into TS do we want to go
+/// it is recommended to maximize TS usage by specifying at least argument and return types, variable types can be inferred
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+        <h1>Hi there!</h1>
+    </div>
+  `)
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
