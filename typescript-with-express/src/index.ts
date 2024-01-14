ccbc933f5b6eea2c173d7bb6fda0e557698ceb41
escobard <escobard90@gmail.com>
2024-01-14 14:20:56 -0700

    build: section 14, lesson 223, add loginRoute route handler

--- typescript-with-express/src/index.ts
+++ typescript-with-express/src/index.ts
@@ -2,11 +2,13 @@
 /// reminder that most libraries need a companion type definition to work with TS
 //// in extreme cases, libraries do not have TS types and we have to build our own
 import express, { Request, Response } from 'express';
+import { router } from "./routes/loginRoutes";
 
 const app = express();
 
 // important to ask - how far into TS do we want to go
 /// it is recommended to maximize TS usage by specifying at least argument and return types, variable types can be inferred
+/*
 app.get('/', (req: Request, res: Response) => {
   res.send(`
     <div>
@@ -14,7 +16,10 @@ app.get('/', (req: Request, res: Response) => {
     </div>
   `)
 })
+*/
+
+app.use(router)
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000')
+  console.log('Listening on port 3000');
 })
