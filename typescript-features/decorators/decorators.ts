1e7260085be78afcab4d16474deeab1ffacfe2d3
escobard <escobard90@gmail.com>
2024-01-15 11:50:09 -0700

    feat: section 15, lesson 240, passes custom argument to decorator, base syntax for declarator factory

--- typescript-features/decorators/decorators.ts
+++ typescript-features/decorators/decorators.ts
@@ -9,7 +9,7 @@ class Boat {
 
   // syntax for declarator function definition
   /// requires experimentalDecorators & emitDecoratorMetadata TS options enabled
-  @logError
+  @logError('Oops, boat was sunk in the ocean')
   pilot(): void {
     throw new Error();
     console.log('swish');
@@ -18,19 +18,39 @@ class Boat {
 
 // syntax for base decorator function
 /// first argument is the prototype of the object, second argument is the key of the property on the object, third argument is the property descriptor
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  // attaches the function body to const method
-  const method = desc.value;
+// function logError(target: any, key: string, desc: PropertyDescriptor): void {
+//   // attaches the function body to const method
+//   const method = desc.value;
+//
+//   // defines another value to the function, intercepting call to the original method
+//   /// original method is still runs as expected
+//   desc.value = function () {
+//     try {
+//       method();
+//     } catch(e){
+//       console.log('Oops boat was sunk')
+//     }
+//   }
+// }
 
-  // defines another value to the function, intercepting call to the original method
-  /// original method is still runs as expected
-  desc.value = function () {
-    try {
-      method();
-    } catch(e){
-      console.log('Oops boat was sunk')
+// syntax for declarator function factory
+/// essentially a higher order function, which passes additional arguments to declarator function
+function logError(errorMessage: string) {
+  return function(target: any, key: string, desc: PropertyDescriptor): void {
+    // attaches the function body to const method
+    const method = desc.value;
+
+    // defines another value to the function, intercepting call to the original method
+    /// original method is still runs as expected
+    desc.value = function () {
+      try {
+        method();
+      } catch(e){
+        console.log(errorMessage)
+      }
     }
   }
 }
+
 
 new Boat().pilot()
