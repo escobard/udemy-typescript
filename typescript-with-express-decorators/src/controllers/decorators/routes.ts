66afea2b9c9e8b78a2a706f4b7797851eb955416
escobard <escobard90@gmail.com>
2024-01-15 18:00:15 -0700

    feat: section 16, lesson 256, routeBinder to avoid having to define one declarator per method

--- typescript-with-express-decorators/src/controllers/decorators/routes.ts
+++ typescript-with-express-decorators/src/controllers/decorators/routes.ts
@@ -1,10 +1,20 @@
 import 'reflect-metadata';
 
 // decorator factory for get paths
-export function get(path: string) {
-  // a tip - using any types is OK for initial function development, but should be swapped out for real types later
-  return function(target: any, key: string, desc: PropertyDescriptor): void {
-    // assigns the path: path key/value metadata to target.key
-    Reflect.defineMetadata('path', path, target, key)
+/// variable decorator factory, wrapped one more function scope, allows us to dynamically define method type for the declarator
+function routeBinder(method: string) {
+  return function(path: string) {
+    // a tip - using any types is OK for initial function development, but should be swapped out for real types later
+    return function(target: any, key: string, desc: PropertyDescriptor): void {
+      // assigns the path: path key/value metadata to target.key
+      Reflect.defineMetadata('path', path, target, key)
+      Reflect.defineMetadata('method', method, target, key)
+    }
   }
 }
+
+// binds method / creates metadata for each HTTP request method
+export const get = routeBinder('get');
+export const post = routeBinder('post');
+export const put = routeBinder('put');
+export const del = routeBinder('delete');
