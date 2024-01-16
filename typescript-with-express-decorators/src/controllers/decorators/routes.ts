a2b5f2a731178bfc0c5ae6749d04765cbe96b3c7
escobard <escobard90@gmail.com>
2024-01-16 15:05:59 -0700

    feat: section 16, lesson 265, add check to ensure only an express RequestHandler type method can be accepted by route method decorator

--- typescript-with-express-decorators/src/controllers/decorators/routes.ts
+++ typescript-with-express-decorators/src/controllers/decorators/routes.ts
@@ -1,13 +1,19 @@
 import 'reflect-metadata';
 import { Methods } from "./Methods";
 import { MetadataKeys } from "./MetadataKeys";
+import { RequestHandler } from "express";
+
+// builds interfaces to ensure decorator only works with express RequestHandler type methods
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler,
+}
 
 // decorator factory for get paths
 /// variable decorator factory, wrapped one more function scope, allows us to dynamically define method type for the declarator
 function routeBinder(method: string) {
   return function(path: string) {
     // a tip - using any types is OK for initial function development, but should be swapped out for real types later
-    return function(target: any, key: string, desc: PropertyDescriptor): void {
+    return function(target: any, key: string, desc: RouteHandlerDescriptor): void {
       // assigns the path: path key/value metadata to target.key
       Reflect.defineMetadata(MetadataKeys.path, path, target, key)
       Reflect.defineMetadata(MetadataKeys.method, method, target, key)
