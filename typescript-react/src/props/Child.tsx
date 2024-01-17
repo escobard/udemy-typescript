583cf2d42427bb1aae68fa2ce08cf58d08db2b37
escobard <escobard90@gmail.com>
2024-01-17 07:11:39 -0700

    doc: section 18, lesson 299, different syntax styles for react components

--- typescript-react/src/props/Child.tsx
+++ typescript-react/src/props/Child.tsx
@@ -7,3 +7,10 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
   return <div>Hi there</div>;
 };
+
+// builds child as a functional react component
+/// this help TS recognize that we are working with React, and pulls in react based types
+//// it is recommended to use this style to maximize TS benefits, but usually the first style is all that is required and has simple syntax
+export const ChildFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>Hi there</div>;
+}
