fce0eb1991797a0f1e985212e2cab48758b39844
escobard <escobard90@gmail.com>
2024-01-30 15:49:33 -0700

    feat: section 17, lesson 279, fetchTodos action reducer

---
+++ typescript-react-redux-classes/src/actions/index.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { Dispatch } from "redux";
+import { ActionTypes } from "./types";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () =>  () => {
+  return async (dispatch: Dispatch) => {
+    // example of how to define returned type annotations for axios.get
+    const response = await axios.get<Todo[]>(url);
+
+    dispatch({
+      type: ActionTypes.fetchTodos,
+      payload: response.data
+    })
+
+  }
+}
---
+++ typescript-react-redux-classes/src/actions/types.ts
@@ -0,0 +1,7 @@
+export enum ActionTypes {
+  // redux only cares about having a unique value
+  /// ts automatically assigns a unique number to each value in an enum, so the shorthand below could be used
+  // fetchTodos
+  //// current version of redux requires value to be a string type
+  fetchTodos = 'FETCH_TODOS'
+}
