af382be059369cd330f04dff7ec3bab868c2ad3e
escobard <escobard90@gmail.com>
2024-01-30 15:33:58 -0700

    feat: section 17, lesson 277, redux setup

--- typescript-react-redux-classes/src/index.tsx
+++ typescript-react-redux-classes/src/index.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
 import ReactDom from "react-dom/client";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { thunk } from 'redux-thunk';
+import { App } from "./components/App";
+import { reducers } from "./reducers";
 
 const el = document.getElementById("root");
 
 const root = ReactDom.createRoot(el!)
 
-interface AppProps {
-  color: string;
-}
-class App extends React.Component<AppProps> {
-  // this is a simplified way of defining state in React classes
-  /// this approach overrides the React type definition for AppProps, avoiding the need to define a ts interface
-  //// it is good practice to pick on approach over the other, I prefer the interface approach for better readability
-  state = { counter: 0 }
+const store = createStore(reducers, applyMiddleware(thunk));
 
-  onIncrement = (): void => {
-    this.setState({counter: this.state.counter + 1});
-  }
-  onDecrement = (): void => {
-    this.setState({counter: this.state.counter - 1});
-  }
-  render(){
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    )
-  }
-}
-
-root.render(<App color="red"/>);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
