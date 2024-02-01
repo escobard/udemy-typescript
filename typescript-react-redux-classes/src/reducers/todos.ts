44970f43c2ccb67a75d5fa3cf0d66c79cbc3e148
escobard <escobard90@gmail.com>
2024-01-31 17:00:01 -0700

    feat: section 17, lesson 288, type guards in reducers

--- typescript-react-redux-classes/src/reducers/todos.ts
+++ typescript-react-redux-classes/src/reducers/todos.ts
@@ -1,11 +1,14 @@
-import { Todo, Action, ActionTypes } from "../actions";
+import {Action, ActionTypes, Todo} from "../actions";
 
 // todosReducer argument sets type annotation and default value
 /// sample reducer with redux and ts
 export const todosReducer = (state: Todo[] = [], action: Action) => {
+
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
